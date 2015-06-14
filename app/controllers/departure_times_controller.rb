893db86816317bfe8d468c71ba63b9c884dad5d6
niconan <nick.hax@gmail.com>
2015-06-14 13:35:40 -0700

    initial distance calculation

--- app/controllers/departure_times_controller.rb
+++ app/controllers/departure_times_controller.rb
@@ -1,8 +1,17 @@
 class DepartureTimesController < ApplicationController
+	include DepartureTimesHelper
 	def index
 		ip = request_ip
 
-		@ip_address = Geocoder.search(ip)
+		@location = Geocoder.search(ip)
+
+		if @location.nil?
+			@lat = 37.362161
+			@long = -121.972694
+		end
+
 		@station_list = Crack::XML.parse(HTTParty.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V').body)
+
+		@distance = distance([@lat, @long], [37.765062, -122.419694])
 	end
 end
--- app/helpers/departure_times_helper.rb
+++ app/helpers/departure_times_helper.rb
@@ -1,2 +1,5 @@
 module DepartureTimesHelper
+  def distance(point_a, point_b)
+    Math.sqrt( (point_b[0] - point_a[0])**2 + (point_b[1] - point_a[1])**2)
+  end
 end
