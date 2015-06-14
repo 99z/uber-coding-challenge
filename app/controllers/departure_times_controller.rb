47bf7a51f9a94e4c2821662c1e01c861cc780b6c
telwell <me@trevorelwell.com>
2015-06-14 16:01:11 -0400

    changing to pg database

--- app/controllers/departure_times_controller.rb
+++ app/controllers/departure_times_controller.rb
@@ -1,8 +1,6 @@
 class DepartureTimesController < ApplicationController
 	def index
-		@ip_address = IP.new.remote_ip(request.remote_ip)
-
+		@ip_address = request.location
 		@station_list = Crack::XML.parse(HTTParty.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V').body)
-		fails
 	end
 end
