fc74ffe922207ce8c3190c523fbbb7de35f548fc
niconan <nick.hax@gmail.com>
2015-06-14 13:17:38 -0700

    ip geolocation working!!!11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def request_ip
+    if Rails.env.development?
+       response = HTTParty.get('http://api.hostip.info/get_html.php')
+       ip = response.split("\n")
+       ip.last.gsub /IP:\s+/, ''
+     else
+       request.remote_ip
+     end
+  end
+
 end
--- app/controllers/departure_times_controller.rb
+++ app/controllers/departure_times_controller.rb
@@ -1,6 +1,8 @@
 class DepartureTimesController < ApplicationController
 	def index
-		@ip_address = request.location
+		ip = request_ip
+
+		@ip_address = Geocoder.search(ip)
 		@station_list = Crack::XML.parse(HTTParty.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V').body)
 	end
 end
