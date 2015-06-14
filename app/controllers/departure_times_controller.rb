a94be2b54ec8132ffeb23cdc28b7f982fe46165c
telwell <me@trevorelwell.com>
2015-06-14 17:59:07 -0400

    fixing distance method and begin closest station method

--- app/controllers/departure_times_controller.rb
+++ app/controllers/departure_times_controller.rb
@@ -1,17 +1,14 @@
 class DepartureTimesController < ApplicationController
 	include DepartureTimesHelper
 	def index
-		ip = request_ip
+		@ip = IP.new
+		@location = Geocoder.search(@ip.remote_ip(request.remote_ip))
 
-		@location = Geocoder.search(ip)
 
-		if @location.nil?
-			@lat = 37.362161
-			@long = -121.972694
-		end
+		@lat = 37.362161
+		@lon = -121.972694
 
 		@station_list = Crack::XML.parse(HTTParty.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V').body)
-
-		@distance = distance([@lat, @long], [37.765062, -122.419694])
+		closest_station(@station_list, @lat, @lon)
 	end
 end
--- app/helpers/departure_times_helper.rb
+++ app/helpers/departure_times_helper.rb
@@ -1,5 +1,14 @@
 module DepartureTimesHelper
-  def distance(point_a, point_b)
-    Math.sqrt( (point_b[0] - point_a[0])**2 + (point_b[1] - point_a[1])**2)
+  def distance(lat1, lon1, lat2, lon2)
+	  Math.sqrt((lat2-lat1)**2 + (lon2-lon1)**2)
+  end
+
+  def closest_station(station_list, lat, lon)
+  	closest_station = {:distance => 0}
+  	station_list['root']['stations']['station'].each do |station|
+  		if distance(@lat, @lon, station['gtfs_latitude'].to_f, station['gtfs_longitude'].to_f) < closest_station[:distance]
+  			closest_station[:distance]
+  		end
+  	end
   end
 end
