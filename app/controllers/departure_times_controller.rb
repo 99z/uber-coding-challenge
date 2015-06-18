b67d537436dec11fc51ea3d73ef5997656051105
niconan <nick.hax@gmail.com>
2015-06-17 21:52:43 -0700

    front end cleanup, automatic form submission on selection

--- app/controllers/departure_times_controller.rb
+++ app/controllers/departure_times_controller.rb
@@ -9,7 +9,7 @@ class DepartureTimesController < ApplicationController
 
 		@station_list = Crack::XML.parse(HTTParty.get('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V').body)
 
-		@station_code = (params[:station_code] ? params[:station_code] : @station_code = closest_station(@station_list, @lat, @lon)['abbr'])
+		@station_code = (params[:station_code] ? params[:station_code] : closest_station(@station_list, @lat, @lon)['abbr'])
 
 		@departures = Crack::XML.parse(HTTParty.get("http://api.bart.gov/api/sched.aspx?cmd=stnsched&orig=#{@station_code}&date=now&key=MW9S-E7SL-26DU-VV8V&l=1").body)
 	end
